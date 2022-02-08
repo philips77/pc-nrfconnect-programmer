825c9f9ae3d63a07ba0cbd45e3eca91804669fbf
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2022-02-08 15:54:26 +0100

    fix: add OpenDialogOptions type
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

--- src/actions/fileActions.ts
+++ src/actions/fileActions.ts
@@ -449,7 +449,8 @@ export const openFileDialog = () => (dispatch: TDispatch) => {
             },
         ],
         properties: ['openFile', 'multiSelections'],
-    };
+        // eslint-disable-next-line no-undef
+    } as Electron.OpenDialogOptions;
     dialog
         .showOpenDialog(dialogOptions)
         .then(
